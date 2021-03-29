78c73a36441b2e76ba409bd2ca36c7ec72b9a16e
acer <tuannguyenduc2k1@gmail.com>
2021-03-29 13:23:02 +0700

    tao sua tao doi tuong

---
+++ app/src/main/java/com/example/myapplication/Model/LoaiDongVat.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.Model;
+
+public class LoaiDongVat {
+    private String loaiDongVat;
+
+    public LoaiDongVat() {
+    }
+
+    public LoaiDongVat(String loaiDongVat) {
+        this.loaiDongVat = loaiDongVat;
+    }
+
+    public String getLoaiDongVat() {
+        return loaiDongVat;
+    }
+
+    public void setLoaiDongVat(String loaiDongVat) {
+        this.loaiDongVat = loaiDongVat;
+    }
+}
